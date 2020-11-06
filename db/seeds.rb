3e0d19bd77fecf17fe6f09a07492f7cd9f427333
maelie poussier <maelie.poussier@icloud.com>
2020-11-06 15:44:01 +0100

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
-
-10.times do
-  article = Article.new(
-    title: Faker::Lorem.sentence,
-    content: Faker::Lorem.paragraph
-  )
-  article.save!
-end
